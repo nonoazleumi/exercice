0f812c6daa1f1a8e3948c68bee48fed2f9f6b810
noemieazerad <noemie.azerad@bankleumi.co.il>
2020-01-19 14:56:55 +0200

    fixed: add click handler to country row item not to textView to make it more comfort to use.

--- app/src/main/java/com/example/myapplication/screen/CountryItemAdapter.kt
+++ app/src/main/java/com/example/myapplication/screen/CountryItemAdapter.kt
@@ -4,6 +4,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication.R
 import com.example.myapplication.network.response.model.Country
@@ -29,7 +30,7 @@ class CountryItemAdapter(private val countryList:List<Country>?, private val lis
 
     class CountryItemViewHolder(itemView: View):RecyclerView.ViewHolder(itemView){
         val countryNameView = itemView.findViewById<TextView>(R.id.country_item_name)
-        val countryCell = itemView.findViewById<TextView>(R.id.country_item_name)
+        val countryCell = itemView.findViewById<ConstraintLayout>(R.id.country_item_constraint)
         val countryNameNativeView = itemView.findViewById<TextView>(R.id.country_item_name_native)
     }
 
